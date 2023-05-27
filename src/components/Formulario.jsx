c9a2064980894c51e4f916204a695c3a07771e52
Caterina Aracil <caterinaaracil@gmail.com>
2023-05-27 16:26:50 +0200

    first commit

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,89 @@
+import { useState, useEffect } from 'react'
+import styled from '@emotion/styled'
+import Error from './Error'
+import useSelectMonedas from '../hooks/useSelectMonedas'
+import { monedas } from '../data/monedas'
+
+const InputSubmit = styled.input`
+    background-color: #9497FF;
+    border: none;
+    width: 100%;
+    padding: 10px;
+    color: #FFF;
+    font-weight: 700;
+    text-transform: uppercase;
+    font-size: 20px;
+    border-radius: 5px;
+    transition: background-color .3s ease;
+    margin-top: 30px;
+    &:hover {
+        background-color: #7A7DFE;
+        cursor: pointer;
+    }
+`
+
+const Formulario = ({setMonedas}) => {
+    const [criptos, setCriptos] = useState([])
+    const [error, setError] = useState(false)
+
+    const [ moneda, SelectMonedas ] = useSelectMonedas('Choose your currency', monedas)
+    const [ criptomoneda, SelectCriptomoneda ] = useSelectMonedas('Choose your cryptocurrency', criptos)
+
+    useEffect(() => {
+        const consultarAPI = async () => {
+            const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD"
+            const respuesta = await fetch(url)
+            const resultado = await respuesta.json()
+
+            const arrayCriptos = resultado.Data.map( cripto => {
+                const objeto = {
+                    id: cripto.CoinInfo.Name,
+                    nombre: cripto.CoinInfo.FullName
+                }
+                return objeto
+            })
+
+            setCriptos(arrayCriptos)
+
+        }
+        consultarAPI();
+    }, [])
+
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        if([moneda, criptomoneda].includes('')) {
+            setError(true)
+            setTimeout(() => {
+                setError(false)
+            }, 3000);
+            return
+        }
+
+        setError(false)
+        setMonedas({
+            moneda,
+            criptomoneda
+        })
+    }
+    
+    return (
+        <>
+            {error && <Error>All fields are mandatory</Error>}
+
+            <form
+                onSubmit={handleSubmit}
+            >
+                <SelectMonedas />
+                <SelectCriptomoneda />
+                
+                <InputSubmit 
+                    type="submit" 
+                    value="To quote" 
+                />
+            </form>
+        </>
+    )
+}
+
+export default Formulario
